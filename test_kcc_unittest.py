fe5171802413527539dcf230fa86114990b92598
Rina Buoy <rinabuoy13@gmail.com>
2020-08-22 22:04:14 +0700

    Add files via upload

---
+++ test_kcc_unittest.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class TestKCC(unittest.TestCase):
+
+    def test_kcc_1(self):
+        t = "ចំណែកជើងទី២ នឹងត្រូវធ្វើឡើងឯប្រទេសកាតា៕"
+        self.assertEqual(segment(t,seg_sep = '-'), 'ចំណែក-ជើង-ទី-២-នឹង-ត្រូវ-ធ្វើឡើង-ឯ-ប្រទេស-កាតា-៕', "ចំណែក-ជើង-ទី-២-នឹង-ត្រូវ-ធ្វើឡើង-ឯ-ប្រទេស-កាតា-៕")
+
+
+
+if __name__ == '__main__':
+    unittest.main()
